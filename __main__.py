87e15b7ac4e6b364fdbbfc17f65de96f4c51bd53
Thomas Roiseux <thomas.roiseux@outlook.com>
2024-05-08 17:18:49 +0200

    Wrote files

--- __main__.py
+++ __main__.py
@@ -6,18 +6,28 @@ from pathlib import Path
 
 FILE_NAME = ".gitignore"
 
+
 async def main() -> None:
     if len(sys.argv) < 2:
         print("Usage: python -m gitignore <language> ...")
         sys.exit(0)
-    
+
     if Path(FILE_NAME).exists():
         print("A .gitignore file already exists in the current directory.")
         sys.exit(0)
-    
+
     args = sys.argv[1:]
-    
-    
-    
+
+    with open(FILE_NAME, "w") as file:
+        try:
+            for result in asyncio.as_completed(
+                [utils.fetch(language) for language in args]
+            ):
+                file.write(await result)
+        except RuntimeError as error:
+            print(error)
+            sys.exit(1)
+
+
 if __name__ == "__main__":
     asyncio.run(main())
