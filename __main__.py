9c7f234c6f4478700c317dae592f9a6f39595e7c
Thomas Roiseux <thomas.roiseux@outlook.com>
2024-05-08 17:30:39 +0200

    Added messages

--- __main__.py
+++ __main__.py
@@ -13,20 +13,30 @@ async def main() -> None:
         sys.exit(0)
 
     if Path(FILE_NAME).exists():
-        print("A .gitignore file already exists in the current directory.")
-        sys.exit(0)
+        if "-f" in sys.argv:
+            Path(FILE_NAME).unlink()
+        else:
+            print("A .gitignore file already exists in the current directory.")
+            sys.exit(0)
 
     args = sys.argv[1:]
 
     with open(FILE_NAME, "w") as file:
+        print(f"Fetching .gitignore files for {', '.join(args)}...")
         try:
             for result in asyncio.as_completed(
-                [utils.fetch(language) for language in args]
+                [
+                    utils.fetch(language)
+                    for language in args
+                    if not language.startswith("-")
+                ]
             ):
                 file.write(await result)
+                file.write("\n")
         except RuntimeError as error:
             print(error)
             sys.exit(1)
+        print(f".gitignore file created successfully.")
 
 
 if __name__ == "__main__":
